811b5d438361705f60886a3b49f1db3f1d202fe8
Jed-Stace <24144js@hvhs.school.nz>
2026-06-21 22:45:41 +0000

    Jun 21, 2026, 10:45 PM

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ console.log("Hello world!")
 function getListFormInput(){
     let userDrinks = Number(document.getElementById("drinksField").value);
 
-    OUTPUT.innerHTML += "<p>You have ordered + i + <p>"
+    OUTPUT.innerHTML += "<p>You have ordered + [userDrinks] + <p>"
 }
